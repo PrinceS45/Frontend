fe50120a8321de3d44adcbe06506bd40ac0927f3
Prince Singh <imprincestark.45@gmail.com>
2025-02-21 14:23:14 +0530

    react days 2

--- react/01vitereact/src/App.jsx
+++ react/01vitereact/src/App.jsx
@@ -1,9 +1,14 @@
 import Chai from "./chai"
 function App() {
-
+const username = 'chai and code' ; 
   return (
+    // hamesha ek hi element return karna hota hai
+    <>
     <Chai/>
-    <h1>E </h1>
+    <h1>Chai and react {username}</h1>
+    <p>test para</p>
+
+    </>
   )
 }
 
--- react/01vitereact/src/main.jsx
+++ react/01vitereact/src/main.jsx
@@ -1,9 +1,40 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react';
+
+function MyApp() {
+   return (
+      <div>
+        <h1>Custom App !</h1>
+      </div>
+   )
+}
+
+// const reactElement = {
+//   type: 'a' , 
+//   props : {
+//          href: 'https://www.google.com' ,
+//          target: 'blank' , 
+//     } ,
+//     children : 'Click me to visite google'
+// }
+
+const anotherElement = (
+   <a href="https://google.com" target='blank'> Visit Google</a>
+)
+
+
+
+const reactElement = React.createElement(
+  'a',
+  { href: 'https://google.com', target: '_blank' },
+  'click me to visit google'
+);
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+    <App/>
+
 )
